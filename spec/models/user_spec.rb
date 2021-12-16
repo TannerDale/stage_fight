6eeb70fd0319687ed2babc421b7e3bb2a0bcb8c3
Tanner Dale <tanner@tannerdale.com>
2021-12-16 17:17:11 -0600

    Add user validations and tests

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe User do
+  describe 'validations' do
+    describe 'presence' do
+      it { should validate_presence_of :username }
+      it { should validate_presence_of :first_name }
+      it { should validate_presence_of :last_name }
+      it { should validate_presence_of :email }
+      it { should validate_presence_of :password }
+      it { should validate_presence_of(:password).on :create }
+    end
+
+    describe 'uniqueness' do
+      it { should validate_uniqueness_of :username }
+      it { should validate_uniqueness_of :email }
+    end
+  end
+end
