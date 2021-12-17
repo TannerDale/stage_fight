4d776bd377aefd5e481b2ab9fc334497cb2e6fb7
Ozzie <57572826+ozzman84@users.noreply.github.com>
2021-12-16 19:27:06 -0700

    Add helpers for user loggedin and authorized

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   add_flash_types :success, :danger
+  helper_method :authorized
+  helper_method :current_user
+  helper_method :logged_in?
 
   include CreationErrorFormatter
+
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def authorized
+    redirect_to welcome_path unless logged_in?
+  end
 end
