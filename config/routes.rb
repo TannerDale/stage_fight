0c1932ae58876a5655fda5c58fa3ec7d4dfef454
Ozzie <57572826+ozzman84@users.noreply.github.com>
2021-12-16 19:03:18 -0700

    Add routes and views for login and register

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :users, only: %i[new create]
 
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+
+  get '/', to: 'sessions#welcome'
+  get 'authorized', to: 'sessions#page_requires_login'
   # Defines the root path route ("/")
   # root "articles#index"
 end
