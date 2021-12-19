924fc8e061d8f5c50957a80c9d9ccc4e5dd1f22b
Tanner Dale <tanner@tannerdale.com>
2021-12-19 15:17:41 -0600

    Remove div created on failed form validations

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,11 @@ module StageFight
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    # Remove 'field_with_errors' div around field when form object has an error
+    config.action_view.field_error_proc = proc { |html, _instance|
+      html
+    }
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
