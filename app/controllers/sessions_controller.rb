916d922189829971587b321b5c018b024e62d1df
Tanner Dale <tanner@tannerdale.com>
2021-12-19 15:16:12 -0600

    Change findby to email, remove empty login action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,17 +2,14 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-    user = User.find_by(username: params[:username])
+    user = User.find_by(email: params[:email])
 
-    if user.authenticate(params[:password])
+    if user&.authenticate(params[:password])
       sessions[:user_id] = user.id
       redirect_to welcome_path, notice: 'Successfully logged in!'
     else
       redirect_to login_path, alert: 'Incorrect email or password, try again.'
     end
-  end
-
-  def login
   end
 
   def destroy
