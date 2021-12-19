844d97d873b6b2cdf001cde605f89690f6945e34
Tanner Dale <tanner@tannerdale.com>
2021-12-19 15:15:11 -0600

    Remove error formatter module and authorize method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,7 @@ class ApplicationController < ActionController::Base
   add_flash_types :success, :danger
   helper_method :current_user
 
-  include CreationErrorFormatter
-
   def current_user
     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
-  end
-
-  def authorize
-    redirect_to login_path unless current_user
   end
 end
