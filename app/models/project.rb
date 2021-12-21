1df509d32f3a6709a122ac9e15197e0a4c037b6e
Tanner Dale <tanner@tannerdale.com>
2021-12-21 17:46:57 -0600

    Add has_many through relationships users <-> cast <-> projects with tests

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,7 @@
 class Project < ApplicationRecord
   validates :title, presence: true
   validates :summary, presence: true
+
+  has_many :casts
+  has_many :users, through: :casts
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,12 +8,17 @@ describe User do
       it { should validate_presence_of :last_name }
       it { should validate_presence_of :email }
       it { should validate_presence_of :password }
-      it { should validate_presence_of(:password).on :create }
+      it { should validate_presence_of(:password_confirmation).on :create }
     end
 
     describe 'uniqueness' do
       it { should validate_uniqueness_of :username }
       it { should validate_uniqueness_of :email }
     end
+  end
+
+  describe 'relationships' do
+    it { should have_many :casts }
+    it { should have_many(:projects).through :casts }
   end
 end
