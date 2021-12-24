585c1f19557885abc2f133ce625633d58ecc035c
Tanner Dale <tanner@tannerdale.com>
2021-12-23 21:25:45 -0600

    remove cast user_id req, add user, project seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,34 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# Reset DB
+User.destroy_all
+Project.destroy_all
+Cast.destroy_all
+ActiveRecord::Base.connection.tables.each do |t|
+  ActiveRecord::Base.connection.reset_pk_sequence!(t)
+end
+
+# Seed Data
+
+[1, 2].each do |num|
+  User.create!(
+    username: "User #{num}",
+    first_name: "FirstName#{num}",
+    last_name: "LastName#{num}",
+    email: "email#{num}@email.com",
+    password: 'password',
+    password_confirmation: 'password',
+    bio: "Awesome user number #{num}"
+  )
+end
+
+[1, 2, 3, 4, 5].each do |num|
+  Project.create!(
+    title: "Project #{num}",
+    summary: "Summary for project #{num}"
+  )
+end
+
+Cast.create!(
+  description: 'hello',
+  project_id: 1,
+  position: 1
+)
